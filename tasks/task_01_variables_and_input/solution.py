3ac27e7b5108739eb59a860295e858a4a4cbf0a0
mungai14 <xaviermungai@gmail.com>
2026-04-24 00:20:00 +0300

    finished task 01

--- tasks/task_01_variables_and_input/solution.py
+++ tasks/task_01_variables_and_input/solution.py
@@ -2,4 +2,18 @@ firstname = " sam "
 lastname = " gitau "
 print( firstname + lastname )
 
+firstname = input( " what is your first_name? " )
+lastname = input( " what is your last_name? " )
+fullname = ( firstname + lastname )
 
+birth_year = input("birth_year: " )
+age = 2026 - int(birth_year)
+print(age) 
+
+print (f"Hello {fullname} you are {age} years old" )
+
+name = "Njau"
+item_price = 133.5
+print(round(item_price,0)) 
+sentence = input("enter a sentence")
+print(f"{name} the bag is {item_price} {sentence} ")
